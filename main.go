7045186f0e71aeadefa480cf0f46fad1df607cca
jollyboss123 <98voonfusing@gmail.com>
2025-03-15 14:29:11 +0800

    get channel conversation history

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/joho/godotenv"
@@ -65,14 +66,69 @@ func main() {
 			return
 		}
 
-		clt.Ack(*evt.Request)
-
-		_, _, err := client.PostMessage(cmd.ChannelID, slack.MsgOptionText("Hello, world!", false))
-		if err != nil {
-			logger.Printf("Error posting message: %v", err)
+		if cmd.Text != "" && !strings.HasPrefix(cmd.Text, "--") {
+			clt.Ack(*evt.Request, map[string]any{
+				"text": "Please provide a valid flag, use /summarize --help for more information",
+			})
 			return
 		}
+
+		if cmd.Text == "" || cmd.Text == "--to-channel" {
+			clt.Ack(*evt.Request)
+
+			msg, err := getChannelHistory(api, cmd.ChannelID)
+			if err != nil {
+				logger.Printf("Error getting channel history: %v", err)
+				return
+			}
+
+			_, _, err = client.PostMessage(cmd.ChannelID, slack.MsgOptionText(strconv.Itoa(len(msg)), false))
+			if err != nil {
+				logger.Printf("Error posting message: %v", err)
+				return
+			}
+			return
+		}
+
+		if cmd.Text == "--to-me" {
+			msg, err := getChannelHistory(api, cmd.ChannelID)
+			if err != nil {
+				logger.Printf("Error getting channel history: %v", err)
+				return
+			}
+
+			clt.Ack(*evt.Request, map[string]any{
+				"text": strconv.Itoa(len(msg)),
+			})
+			return
+		}
+
+		if cmd.Text == "--help" {
+			clt.Ack(*evt.Request, map[string]any{
+				"text": `Usage: /summarize [flag]
+--to-channel: Send message to channel
+--to-me: Send message to me
+`,
+			})
+			return
+		}
+
+		clt.Ack(*evt.Request, map[string]any{
+			"text": "Invalid flag, use /summarize --help for more information",
+		})
 	})
 
 	socketmodeHandler.RunEventLoop()
+}
+
+func getChannelHistory(api *slack.Client, channelID string) ([]slack.Message, error) {
+	history, err := api.GetConversationHistory(&slack.GetConversationHistoryParameters{
+		ChannelID: channelID,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return history.Messages, nil
+
 }
